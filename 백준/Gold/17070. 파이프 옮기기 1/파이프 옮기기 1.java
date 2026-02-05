9b7a9b15a02d768138edde5fa4cf9a7cb2ba7511
Jin <146903649+jinkong9@users.noreply.github.com>
2026-02-05 13:07:45 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 292 ms, Memory: 19880 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,51 @@
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int dr[] = { 1, 1, 0 };
+	static int dc[] = { 0, 1, 1 };
+	static int arr[][];
+	static boolean v[][];
+	static int T, cnt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		arr = new int[T][T];
+
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < T; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		v = new boolean[T][T];
+		dfs(0, 1, 2);
+		System.out.println(cnt);
+	}
+
+	static void dfs(int r, int c, int d) {
+		if (r == T - 1 && c == T - 1) {
+			cnt++;
+			return;
+		}
+		
+		for (int i = 0; i < 3; i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			
+			if (d == 0 && i == 2) continue;
+			if(d == 2 && i == 0) continue;
+			if (nr >= 0 && nr < T && nc >= 0 && nc < T && !v[nr][nc] && arr[nr][nc] == 0) {
+				if(i ==1) {
+					if(arr[r+1][c] == 1 || arr[r][c+1] == 1 || arr[r+1][c+1] == 1) continue;
+				}
+				v[nr][nc] = true;
+				dfs(nr,nc, i);
+				v[nr][nc] = false;
+			}
+		}
+	}
+}
