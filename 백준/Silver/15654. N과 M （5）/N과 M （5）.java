8e450eff40e79b9f98fd9592d23ec7bff883b1f9
Jin <146903649+jinkong9@users.noreply.github.com>
2026-02-08 20:30:54 +0900

    [Silver III] Title: N과 M (5), Time: 308 ms, Memory: 29156 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,46 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int A,B, arr[];
+	static boolean v[];
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		arr = new int[A];
+		v = new boolean[A];
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		for(int i=0; i<A; i++) {
+			arr[i] = Integer.parseInt(st1.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		dfs(0, new int[B]);
+		System.out.println(sb);
+	}
+	
+	static void dfs(int idx, int sel[]) {
+		if(idx == B) {
+			for(int i=0; i<B; i++) {
+				sb.append(sel[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i =0; i<arr.length; i++) {
+			if(!v[i]) {
+				sel[idx] = arr[i];
+				v[i] = true;
+				dfs(idx+1, sel);
+				v[i] = false;
+			}
+		}
+	}
+	
+}
