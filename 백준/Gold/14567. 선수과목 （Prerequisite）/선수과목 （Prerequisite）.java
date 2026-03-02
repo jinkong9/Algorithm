ddf6a59b089171f46418022ef7aaf75baa86943d
Jin <146903649+jinkong9@users.noreply.github.com>
2026-03-02 17:42:54 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 548 ms, Memory: 129840 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,60 +1,59 @@
 
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	static int N, M;
 	static ArrayList<Integer> list[];
-	static int ind[];
+	static int ind[], N;
 	static StringBuilder sb;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		list = new ArrayList[N + 1];
-		ind = new int[N + 1];
-
-		for (int i = 1; i <= N; i++) {
+		int M = Integer.parseInt(st.nextToken());
+		list = new ArrayList[N+1];
+		ind = new int[N+1];
+		
+		for(int i=1; i<=N; i++) {
 			list[i] = new ArrayList<>();
 		}
-		for (int i = 0; i < M; i++) {
+		
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
+			
 			list[A].add(B);
-			ind[B]++;
+			ind[B] ++;
 		}
 		sb = new StringBuilder();
-		dfs();
+		bfs();
 		System.out.println(sb);
 	}
 
-	static void dfs() {
-		int res[] = new int[N + 1];
-		Queue<Integer> Q = new ArrayDeque<>();
-
-		for (int i = 1; i <= N; i++) {
-			if (ind[i] == 0) {
+	static void bfs() {
+		int res[] = new int[N+1];
+		Queue <Integer> Q = new ArrayDeque<>();
+		
+		for(int i=1; i<=N; i++) {
+			if(ind[i] == 0) {
 				res[i] = 1;
 				Q.offer(i);
 			}
 		}
-		while (!Q.isEmpty()) {
+		
+		while(!Q.isEmpty()) {
 			int now = Q.poll();
-
-			for (int i : list[now]) {
-				ind[i]--;
-				res[i] = Math.max(res[i], res[now] + 1);
-
-				if (ind[i] == 0) {
+			for(int i : list[now]) {
+				ind[i] --;
+				res[i] = Math.max(res[i], res[now] +1);
+				
+				if(ind[i] == 0) {
 					Q.offer(i);
 				}
 			}
 		}
-		for (int i = 1; i <= N; i++) {
+		
+		for(int i=1; i<=N; i++) {
 			sb.append(res[i]).append(" ");
 		}
 	}
