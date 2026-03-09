d70d5f80bc058019c2076793c99de66d5d4d041c
Jin <146903649+jinkong9@users.noreply.github.com>
2026-03-09 16:17:50 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 332 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,105 +1,105 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, D;
-    static int[][] origin;
-    static int answer = 0;
+	static int N, M, D, arr[][], bowR[], bowC[], ans;
 
-    static class Enemy {
-        int r, c, d;
-        Enemy(int r, int c, int d) {
-            this.r = r;
-            this.c = c;
-            this.d = d;
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		ans = 0;
+		comb(0, 0, new int[3]);
+		System.out.println(ans);
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	static void comb(int idx, int start, int sel[]) {
+		if (idx == 3) {
+			play(sel);
+			return;
+		}
+		// 조합 
+		for (int i = start; i < M; i++) {
+			sel[idx] = i;
+			comb(idx + 1, i + 1, sel);
+		}
+	}
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
+	static void play(int sel[]) {
+		int map[][] = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			map[i] = arr[i].clone();
+		}
+		int tmp = 0;
+		while (true) {
+			boolean k[][] = new boolean[N][M];
+			for (int a = 0; a < sel.length; a++) {
+				int ar = N;
+				int ac = sel[a];
 
-        origin = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                origin[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+				int tr = -1;
+				int tc = -1;
+				// target r, target c임
+				int min = Integer.MAX_VALUE;
 
-        // 궁수 위치 3개 조합
-        for (int i = 0; i < M - 2; i++) {
-            for (int j = i + 1; j < M - 1; j++) {
-                for (int k = j + 1; k < M; k++) {
-                    simulate(new int[]{i, j, k});
-                }
-            }
-        }
+				for (int r = N - 1; r >= 0; r--) {
+					for (int c = 0; c < M; c++) {
+						if (map[r][c] == 1) {
+							// 거리계산
+							int d = Math.abs(ar - r) + Math.abs(ac - c);
+							// 최소 거리보다 크면은 넘김
+							if (d > D)
+								continue;
+							// 최소거리거나 더 왼쪽에 있을 때 경우
+							if (d < min || d == min && c < tc) {
+								min = d;
+								tr = r;
+								tc = c;
+								// map이 1일 때 즉, 적이 있을 때 tr tc 바꿔줌
+							}
+						}
+					}
+				}
+				if (tr != -1)
+					k[tr][tc] = true;
+				// 적이 있으면 tr, tc가 바뀌므로 true
+			}
+			for (int r = N - 1; r >= 0; r--) {
+				for (int c = 0; c < M; c++) {
+					if (k[r][c]) {
+						map[r][c] = 0;
+						tmp++;
+					}
+				}
+			}
+			boolean flag = true;
+			for (int r = N - 1; r > 0; r--) {
+				map[r] = map[r - 1].clone();
+			}
+			map[0] = new int[M];
 
-        System.out.println(answer);
-    }
-
-    static void simulate(int[] archers) {
-        int[][] map = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            map[i] = origin[i].clone();
-        }
-
-        int kill = 0;
-
-        for (int turn = 0; turn < N; turn++) {
-            Set<String> targets = new HashSet<>();
-
-            // 각 궁수별 타겟 탐색
-            for (int a : archers) {
-                Enemy target = findTarget(map, a);
-                if (target != null) {
-                    targets.add(target.r + "," + target.c);
-                }
-            }
-
-            // 동시 제거
-            for (String s : targets) {
-                String[] t = s.split(",");
-                int r = Integer.parseInt(t[0]);
-                int c = Integer.parseInt(t[1]);
-                if (map[r][c] == 1) {
-                    map[r][c] = 0;
-                    kill++;
-                }
-            }
-
-            // 적 한 칸 아래로 이동
-            for (int r = N - 1; r > 0; r--) {
-                map[r] = map[r - 1];
-            }
-            map[0] = new int[M];
-        }
-
-        answer = Math.max(answer, kill);
-    }
-
-    static Enemy findTarget(int[][] map, int archerCol) {
-        Enemy res = null;
-
-        for (int r = N - 1; r >= 0; r--) {
-            for (int c = 0; c < M; c++) {
-                if (map[r][c] == 1) {
-                    int dist = Math.abs(N - r) + Math.abs(archerCol - c);
-                    if (dist <= D) {
-                        if (res == null ||
-                            dist < res.d ||
-                            (dist == res.d && c < res.c)) {
-                            res = new Enemy(r, c, dist);
-                        }
-                    }
-                }
-            }
-        }
-        return res;
-    }
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if (map[i][j] == 1) {
+						flag = false;
+						break;
+					}
+				}
+			}
+			if (flag) {
+				ans = Math.max(ans, tmp);
+				break;
+			}
+		}
+	}
 }
