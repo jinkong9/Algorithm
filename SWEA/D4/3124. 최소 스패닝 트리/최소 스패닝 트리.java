be6204a3c952601c68f66e7433f553720da788c1
Jin <146903649+jinkong9@users.noreply.github.com>
2026-03-11 09:08:36 +0900

    [D4] Title: 최소 스패닝 트리, Time: 3,069 ms, Memory: 192,044 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,81 +1,78 @@
 
-
 import java.io.*;
 import java.util.*;
 
 public class Solution {
+	static int N, M;
+	static ArrayList<Ver> list[];
+	static boolean v[];
+	static class Ver implements Comparable<Ver> {
+		int to;
+		long w;
+		Ver(int to, long w) {
+			this.to = to;
+			this.w = w;
+		}
+		@Override
+		public int compareTo(Ver o) {
+			return Long.compare(this.w, o.w);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			v = new boolean[N+1];
+			list = new ArrayList[N+1];
+			
+			for(int i=1; i<=N; i++) list[i] = new ArrayList<>();
+			
+			for(int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				long w = Integer.parseInt(st.nextToken());
+				
+				list[a].add(new Ver(b,w));
+				list[b].add(new Ver(a,w));
+			}
+			
+			long ans = prim();
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	static long prim() {
+		PriorityQueue<Ver> Q = new PriorityQueue<>();
+		Q.offer(new Ver(1,0));
+		
+		long res = 0;
+		int cnt = 0;
+		
+		while(!Q.isEmpty()) {
+			Ver V = Q.poll();
+			
+			if(v[V.to]) continue;
+			v[V.to] = true;
+			res += V.w;
+			cnt ++; 
+			
+			if(cnt == N) break;
+			
+			for(Ver next : list[V.to]) {
+				if(!v[next.to]) {
+					Q.offer(next);
+				}
+			}
+		}
+		return res;
+	}
 
-    static int N,M;
-    static int parent[];
-
-    static class Ver {
-        int from, to, w;
-
-        Ver(int from, int to, int w){
-            this.from = from;
-            this.to = to;
-            this.w = w;
-        }
-
-    }
-
-    static int find(int a){
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
-    }
-
-    static void union(int a, int b){
-        a = find(a);
-        b = find(b);
-
-        if(a != b) {
-        	parent[a] = b;
-        }
-    }
-
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine());
-
-        for(int tc=1; tc<=T; tc++){
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            
-            
-            parent = new int[N+1];
-            for(int i=1;i<=N;i++) parent[i] = i;
-
-            ArrayList<Ver> list = new ArrayList<>();
-
-            for(int i=0; i<M; i++) {
-            	st = new StringTokenizer(br.readLine());
-            	int x = Integer.parseInt(st.nextToken());
-            	int y = Integer.parseInt(st.nextToken());
-            	int w = Integer.parseInt(st.nextToken());
-            	list.add(new Ver(x,y,w));
-            }
-            
-            Collections.sort(list, (a,b) -> a.w - b.w);
-
-            long sum = 0;
-            int cnt = 0;
-            for(Ver v : list){
-                if(find(v.from) != find(v.to)){
-                	union(v.from,v.to);
-                	sum += v.w;
-                    cnt ++;
-                    if(cnt == N-1) break;
-                }
-            }
-
-
-            StringBuilder sb = new StringBuilder();
-            sb.append("#").append(tc).append(" ").append(sum);
-            System.out.println(sb);
-        }
-    }
 }
