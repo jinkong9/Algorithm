cdf25de85bb1e484a2ce6b631be9313dafe7b488
Jin <146903649+jinkong9@users.noreply.github.com>
2026-09-21 15:25:29 +0900

    [level 2] Title: 멀리 뛰기, Time: 8.92 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2; i<=n; i++) {
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+        }
+        
+        for(int i=1; i<=n; i++) System.out.println(dp[i]);
+        
+        answer = dp[n];
+        return answer;
+    }
+}
